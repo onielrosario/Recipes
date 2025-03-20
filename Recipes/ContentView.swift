f5aff6926487535fc1d929884515557c8775bfcb
Oniel <orose2689@gmail.com>
2025-03-20 13:20:03 -0400

    Initial Commit

---
+++ Recipes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Recipes
+//
+//  Created by oniel rosario on 3/20/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RecipesTests/RecipesTests.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesTests.swift
+//  RecipesTests
+//
+//  Created by oniel rosario on 3/20/25.
+//
+
+import Testing
+@testable import Recipes
+
+struct RecipesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
